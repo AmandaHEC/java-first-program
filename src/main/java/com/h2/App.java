bbe1901f56a6d46ff31324c56c2116f1f97346f4
Amanda Stokes <williford.mandy@gmail.com>
2022-10-19 09:15:46 -0500

    implementedMethods

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -8,10 +8,19 @@ public class App
 {
     public static void main( String[] args )
     {
+
         System.out.println( "Hello World!" );
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return 2 * number;
+    }
+
+    private static int add(int[] numbers){
+        int sum = 0;
+        for(int number : numbers){
+        sum += number;
+        }
+        return sum;
     }
 }
